526442511823ebe5ed235666083df4640f03ffb2
test1 <abhijeet@skilleagle.com>
2014-11-25 01:01:34 -0500

    Added getters and setters to Class A

--- src/com/example/gittrial/ClassA.java
+++ src/com/example/gittrial/ClassA.java
@@ -1,5 +1,22 @@
 package com.example.gittrial;
 
 public class ClassA {
-
+	int a;
+	double b;
+	
+	public int getA() {
+		return a;
+	}
+	
+	public void setA(int a) {
+		this.a = a;
+	}
+	
+	public double getB() {
+		return b;
+	}
+	
+	public void setB(double b) {
+		this.b = b;
+	}
 }
